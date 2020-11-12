8ba3f78fb505f6ec171b97613280a80b2fb62b49
ryan <ryan.hill1@mydit.ie>
2020-11-12 10:39:51 +0000

    adding first component

---
+++ src/Components/MyFirstComponent.js
@@ -0,0 +1,11 @@
+function MyFirstComponent() {
+    return (
+        <div className="MyFirstComponent">
+                <p>
+                    My second Component
+                </p>
+        </div>
+    );
+}
+
+export default MyFirstComponent;
